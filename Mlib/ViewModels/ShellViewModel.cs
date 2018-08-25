1284b2fc56f5c3e8d03e0ddd8fdcf461c2df5192
Kamil <k.nowak3003@gmail.com>
2018-08-25 20:14:05 +0200

    ui

--- Mlib/ViewModels/ShellViewModel.cs
+++ Mlib/ViewModels/ShellViewModel.cs
@@ -27,10 +27,40 @@ namespace Mlib.ViewModels
             var screenWidth = SystemParameters.PrimaryScreenWidth;
             var screenHeight = SystemParameters.PrimaryScreenHeight;
             view.Width = screenWidth * windowWidthScale;
-            view.Height = screenHeight * windowHeightScale ;
+            view.Height = screenHeight * windowHeightScale;
 
             view.Left = (screenWidth / 2) - (view.Width / 2);
             view.Top = (screenHeight / 2) - (view.Height / 2);
         }
+        public void Close()
+        {
+            Application.Current.Shutdown();
+        }
+        private Point startPoint;
+        public void Drag(Window view)
+        {
+            view.DragMove();
+        }
+        //private void toggleButton_PreviewMouseLeftButtonDown(
+        //    object sender, MouseButtonEventArgs e)
+        //{
+        //    startPoint = e.GetPosition(toggleButton);
+        //}
+
+        //private void toggleButton_PreviewMouseMove(object sender, MouseEventArgs e)
+        //{
+        //    var currentPoint = e.GetPosition(toggleButton);
+        //    if (e.LeftButton == MouseButtonState.Pressed &&
+        //        toggleButton.IsMouseCaptured &&
+        //        (Math.Abs(currentPoint.X - startPoint.X) >
+        //            SystemParameters.MinimumHorizontalDragDistance ||
+        //        Math.Abs(currentPoint.Y - startPoint.Y) >
+        //            SystemParameters.MinimumVerticalDragDistance))
+        //    {
+        //        // Prevent Click from firing
+        //        toggleButton.ReleaseMouseCapture();
+        //        DragMove();
+        //    }
+        //}
     }
 }
