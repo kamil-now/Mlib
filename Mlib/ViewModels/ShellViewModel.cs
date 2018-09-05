6423b86277115a5ed41f3f6f77b5fc517233273b
Kamil <k.nowak3003@gmail.com>
2018-09-05 19:31:50 +0200

    ShellView window behavior methods

--- Mlib/ViewModels/ShellViewModel.cs
+++ Mlib/ViewModels/ShellViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using Caliburn.Micro;
 using Mlib.UI.ViewModels.Interfaces;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Mlib.ViewModels
 {
     public class ShellViewModel : Screen
     {
+        private bool mRestoreForDragMove;
         private Window window;
         private IMainViewModel mainView;
+
         public IMainViewModel MainView
         {
             get => mainView;
@@ -17,29 +20,76 @@ namespace Mlib.ViewModels
                 NotifyOfPropertyChange();
             }
         }
+        public bool IsMaximized => window?.WindowState == WindowState.Maximized;
+
         public ShellViewModel(IMainViewModel main)
         {
             mainView = main;
         }
+      
         public void OnLoad(Window window)
         {
             this.window = window;
+            window.Style = (Style)window.FindResource(typeof(Window));
+
+            SetWindowSize();
+            SetWindowMargin();
+
+            window.StateChanged += (s, e) => NotifyOfPropertyChange(() => IsMaximized);
+        }
+
+        public void Drag(MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (window.ResizeMode != ResizeMode.CanResize && window.ResizeMode != ResizeMode.CanResizeWithGrip)
+                {
+                    return;
+                }
+
+                ToggleWindowState();
+            }
+
+            else
+            {
+                mRestoreForDragMove = IsMaximized;
+                window.DragMove();
+            }
+
+        }
+        public void MouseMove(MouseEventArgs e)
+        {
+            if (mRestoreForDragMove)
+            {
+                mRestoreForDragMove = false;
+
+                var point = window.PointToScreen(e.MouseDevice.GetPosition(window));
+
+                window.Left = point.X - (window.RestoreBounds.Width * 0.5);
+                window.Top = point.Y;
+
+                window.WindowState = WindowState.Normal;
+
+                window.DragMove();
+            }
+        }
+        public void MouseUp() => mRestoreForDragMove = false;
+        public void ToggleWindowState() => window.WindowState = IsMaximized ? WindowState.Normal : WindowState.Maximized;
+        public void Minimize() => window.WindowState = WindowState.Minimized;
+        public void Close() => Application.Current.Shutdown();
+
+        private void SetWindowSize()
+        {
             var windowHeightScale = 0.8;//TODO external setting
             var windowWidthScale = 0.82;//TODO external setting
-            var screenWidth = SystemParameters.PrimaryScreenWidth;
-            var screenHeight = SystemParameters.PrimaryScreenHeight;
-            window.Width = screenWidth * windowWidthScale;
-            window.Height = screenHeight * windowHeightScale;
 
-            window.Left = (screenWidth / 2) - (window.Width / 2);
-            window.Top = (screenHeight / 2) - (window.Height / 2);
+            window.Width = SystemParameters.PrimaryScreenWidth * windowWidthScale;
+            window.Height = SystemParameters.PrimaryScreenHeight * windowHeightScale;
         }
-        public void Close()
+        private void SetWindowMargin()
         {
-            Application.Current.Shutdown();
+            window.Left = (SystemParameters.PrimaryScreenWidth / 2) - (window.Width / 2);
+            window.Top = (SystemParameters.PrimaryScreenHeight / 2) - (window.Height / 2);
         }
-        public void Drag() => window.DragMove();
-
-        public void Minimize() => window.WindowState = WindowState.Minimized;
     }
 }
