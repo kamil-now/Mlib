6872d7267f29e8f4111d006ea73df3a6ec7458d0
Kamil <k.nowak3003@gmail.com>
2018-11-27 14:46:21 +0100

    better drag drop visuals, audioplayer current playlist reorder

--- Mlib/UI/ViewModels/PlaylistViewModel.cs
+++ Mlib/UI/ViewModels/PlaylistViewModel.cs
@@ -14,8 +14,9 @@
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
+    using System.Windows.Media;
 
-    public class PlaylistViewModel : Screen, IViewModel, IDragSource
+    public class PlaylistViewModel : Screen, IViewModel, IDropTarget, IDragSource
     {
         public AudioPlayer AudioPlayer { get; }
         public ObservableCollection<Track> Tracks
@@ -24,6 +25,7 @@
             set
             {
                 _tracks = value;
+                AudioPlayer.CurrentPlaylist.Tracks = value;
                 NotifyOfPropertyChange();
             }
         }
@@ -33,7 +35,79 @@
             Tracks = new ObservableCollection<Track>(AudioPlayer.CurrentPlaylist.Tracks);
         }
         private ObservableCollection<Track> _tracks;
-      
+
+
+        public ICommand Select => new Command(track =>
+        {
+            AudioPlayer.Play(track as Track);
+        });
+        Separator previous;
+        public void DragOver(IDropInfo dropInfo)
+        {
+            dropInfo.Effects = DragDropEffects.Move;
+            var dragOverBrush = Application.Current.FindResource("GlobalAccent") as SolidColorBrush;
+            var defaultBrush = Application.Current.FindResource("GlobalLightDim") as SolidColorBrush;
+            Separator separator;
+
+            separator = FindVisualChild<Separator>(dropInfo.VisualTargetItem as ListViewItem);
+            if (separator != null)
+                separator.BorderBrush = dragOverBrush;
+            if (previous != null && previous != separator)
+            {
+                previous.BorderBrush = defaultBrush;
+            }
+            previous = separator;
+        }
+        public static T FindVisualChild<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        return (T)child;
+                    }
+
+                    T childItem = FindVisualChild<T>(child);
+                    if (childItem != null) return childItem;
+                }
+            }
+            return null;
+        }
+        public static T FindVisualParent<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                DependencyObject parent = VisualTreeHelper.GetParent(depObj);
+                if (parent != null && parent is T)
+                {
+                    return (T)parent;
+                }
+
+                T parentItem = FindVisualParent<T>(parent);
+                if (parentItem != null) return parentItem;
+
+            }
+            return null;
+        }
+        public void Drop(IDropInfo dropInfo)
+        {
+            Track sourceItem = dropInfo.Data as Track;
+            Track targetItem = dropInfo.TargetItem as Track;
+            if (targetItem != null && sourceItem != null)
+            {
+
+                var targetIndex = Tracks.IndexOf(Tracks.First(n => n.Id == targetItem.Id));
+                var sourceIndex = Tracks.IndexOf(Tracks.First(n => n.Id == sourceItem.Id));
+
+                Tracks.RemoveAt(sourceIndex);
+                Tracks.Insert(targetIndex, sourceItem);
+                var separator = FindVisualChild<Separator>(dropInfo.VisualTargetItem as ListViewItem);
+                separator.BorderBrush = Application.Current.FindResource("GlobalLightDim") as SolidColorBrush;
+            }
+        }
         public void StartDrag(IDragInfo dragInfo)
         {
             Track track = (Track)dragInfo.SourceItem;
@@ -45,27 +119,22 @@
 
         public bool CanStartDrag(IDragInfo dragInfo)
         {
-            throw new NotImplementedException();
+            return true;//throw new NotImplementedException();
         }
 
         public void Dropped(IDropInfo dropInfo)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void DragCancelled()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public bool TryCatchOccurredException(Exception exception)
         {
-            throw new NotImplementedException();
+            return false;//throw new NotImplementedException();
         }
-        
-        public ICommand Select => new Command(track =>
-        {
-            AudioPlayer.Play(track as Track);
-        });
     }
 }
